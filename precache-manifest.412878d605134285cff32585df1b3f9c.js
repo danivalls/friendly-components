5573c9824b4436543030f66f490779542f92cd43
Daniel Valls <daniel.valls.mestre@gmail.com>
2021-12-06 20:31:55 +0100

    Updates

---
+++ precache-manifest.412878d605134285cff32585df1b3f9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7fc1e07eca7551ed5a1248772fbb3d",
+    "url": "/friendly-components/index.html"
+  },
+  {
+    "revision": "55a577abf55ca09b00e5",
+    "url": "/friendly-components/static/css/main.3fe8223f.chunk.css"
+  },
+  {
+    "revision": "878e683bf377bf7be169",
+    "url": "/friendly-components/static/js/2.3bc71735.chunk.js"
+  },
+  {
+    "revision": "3ebbc1b45bd365e8d636e57a42f7f70a",
+    "url": "/friendly-components/static/js/2.3bc71735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55a577abf55ca09b00e5",
+    "url": "/friendly-components/static/js/main.53b2dee8.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "/friendly-components/static/js/main.53b2dee8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "844d365ae099ccab4610",
+    "url": "/friendly-components/static/js/runtime-main.376e5fc9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d6968c392559e805c96f6c6a12528613.js"
+  "/friendly-components/precache-manifest.412878d605134285cff32585df1b3f9c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/friendly-components/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
